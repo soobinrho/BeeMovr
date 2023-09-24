7ec10eae527c28c2f54dc55dcfa813ed3b4e979e
Soobin Rho <soobinrho@gmail.com>
2023-09-24 17:00:06 -0500

    refactor: GET API's structure (WIP)
    Source:
      https://nextjs.org/docs/app/building-your-application/routing/route-handlers
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/app/v1/prediction/honey-yield/route.ts
+++ src/app/v1/prediction/honey-yield/route.ts
@@ -1,4 +1,17 @@
-export async function GET(request: Request) {}
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+    // const res = await fetch('https://...' {
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+    // const data = await res.json()
+    // return NextResponse.json({ data })
+    return NextResponse.json({
+        'TestName': 'TestValue'
+    })
+}
 
 // TODO: Use querying to get honey yield prediction values.
 // e.g. https://beemovr.com/v1/honey?precipitation=123&max-temp=12&min-temp=0
--- src/app/v1/weather/max-temp/route.ts
+++ src/app/v1/weather/max-temp/route.ts
@@ -1,1 +1,14 @@
-export async function GET(request: Request) {}
+import { NextResponse } from 'next/server'
+
+export async function GET(request: Request) {
+    // const res = await fetch('https://...' {
+    //     headers: {
+    //         'Content-Type': 'application/json'
+    //     }
+    // })
+    // const data = await res.json()
+    // return NextResponse.json({ data })
+    return NextResponse.json({
+        'TestName': 'TestValue'
+    })
+}
