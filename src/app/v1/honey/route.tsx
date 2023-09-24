0df59d2654ebe74f4bc0afce5d47b3baab087397
Soobin Rho <soobinrho@gmail.com>
2023-09-24 13:50:00 -0500

    docs: TODO: use querying for honey GET API's
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/app/v1/honey/route.tsx
+++ src/app/v1/honey/route.tsx
@@ -1,1 +1,4 @@
 export async function GET(request: Request) {}
+
+// TODO: Use querying to get honey yield prediction values.
+// e.g. https://beemovr.com/v1/honey?precipitation=123&max-temp=12&min-temp=0
