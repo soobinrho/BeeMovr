145bf591fedd03d3306131f5474505e266bbf49a
Soobin Rho <soobinrho@gmail.com>
2023-09-30 23:40:19 -0500

    refactor: honey yield prediction calculation (WIP)
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/app/v1/prediction/honey-yield/calculate-honey-yield.ts
+++ src/app/v1/prediction/honey-yield/calculate-honey-yield.ts
@@ -28,8 +28,26 @@ export function calculateHoneyYield(): string {
   // const api_start_date = start_date.toISOString().split("T")[0];
   // const api_end_date = end_date.toISOString().split("T")[0];
 
-  return '0';
+  // Honey yield prediction model.
+  // Source:
+  //   "The Impact of Precipitation and Temperature on Honey Yield in
+  //   the United States." 2020. Auburn University. Hayes Kent Grogan.
+  const prediction_honeyYield = (
+    (60.596) +
+    (0.001 * monthly_precipitation[2] * 10) +
+    (-0.001 * monthly_precipitation[1] * 10) +
+    (0.056 * monthly_minTemp[2] * 10) +
+    (0.027 * monthly_minTemp[1] * 10) +
+    (-0.027 * monthly_minTemp[0] * 10) +
+    (-0.034 * monthly_maxTemp[2] * 10) +
+    (0.012 * monthly_maxTemp[1] * 10) +
+    (0.032 * monthly_maxTemp[0] * 10) +
+    (0.465 * 0.074 + 2.28 * 0.012 + 9.679 * 0.04)
+  );
+  
+  return prediction_honeyYield;
 }
 
 // TODO: Do more domain research on nectar availability:
 //   https://scholar.google.com/scholar?hl=en&as_sdt=0%2C8&q=nectar+availability&btnG=&oq=nec
+// TODO: Show the confidence level of predictions to always inform.
