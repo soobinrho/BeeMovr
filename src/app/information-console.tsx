bd0d1be656dba9d1037f53c30f5f8b4d5550a1ab
Soobin Rho <soobinrho@gmail.com>
2023-10-07 11:39:01 -0500

    refactor: enable information console for mobile

--- src/app/information-console.tsx
+++ src/app/information-console.tsx
@@ -1,7 +1,7 @@
 export default function InformationConsole() {
   return (
-    <div className='invisible absolute bottom-4 z-10 w-screen sm:visible'>
-      <div className='mx-2 my-5 flex flex-col-reverse justify-self-start rounded-md bg-background-console/50 p-5 text-left font-semibold text-font-console hover:mx-0 hover:rounded-none hover:bg-background-console/90 hover:px-7'>
+    <div className='absolute bottom-4 z-10 w-screen'>
+      <div className='mx-2 my-12 flex flex-col-reverse justify-self-start overflow-x-auto rounded-md bg-background-console/50 p-5 text-left font-semibold text-font-console hover:mx-0 hover:rounded-none hover:bg-background-console/90 hover:px-7 sm:my-5'>
         <p>
           <b>Test value</b> {process.env.NEXT_PUBLIC_MAPBOX_API_TOKEN} <br />
           <b>Test value</b> {process.env.NEXT_PUBLIC_URL} <br />
