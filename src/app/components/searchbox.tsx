9ade948fc0f66ca0d0df8276b7f67990ce68e1ea
Soobin Rho <soobinrho@gmail.com>
2023-10-20 00:16:47 -0500

    refactor: mobile screen size search box component
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/app/components/searchbox.tsx
+++ src/app/components/searchbox.tsx
@@ -18,6 +18,7 @@ export default function Searchbox() {
   const { mapMain } = useMap();
   const [searchValue, setSearchValue] = useState<string | null>(null);
   const [shouldFetch, setShouldFetch] = useState(false);
+  const [shouldOpenSearchBox, setShouldOpenSearchBox] = useState(false);
 
   const { data, error } = useSWRImmutable(
     shouldFetch
@@ -99,40 +100,82 @@ export default function Searchbox() {
     }
   }, [searchValue]);
 
-  // TODO: Create a separate, but almost identical search box component for mobile.
   return (
-    <div className='absolute start-0 z-10 ml-4 mt-6 flex w-[28%] flex-auto flex-grow translate-x-0 flex-row flex-nowrap items-center gap-0 pl-3 pt-1 sm:start-[50%] sm:ml-5 sm:mt-7 sm:translate-x-[-50%] sm:pt-0'>
-      <div className='start-0 z-10 translate-x-0 text-white/50 sm:start-[50%] sm:translate-x-[-50%]'>
-        <button onClick={onSubmit} className='mt-2'>
-          <svg
-            className='fill-current text-font-console hover:text-white active:text-gray-300'
-            xmlns='http://www.w3.org/2000/svg'
-            width='24'
-            height='24'
-            viewBox='0 0 24 24'
-          >
-            <path d='M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z' />
-          </svg>
-        </button>
+    <>
+      <div className='invisible absolute start-0 z-10 ml-4 mt-6 flex w-[28%] flex-auto flex-grow translate-x-0 flex-row flex-nowrap items-center gap-0 pl-3 pt-1 sm:visible sm:start-[50%] sm:ml-5 sm:mt-7 sm:translate-x-[-50%] sm:pt-0'>
+        <div className='start-0 z-10 translate-x-0 text-white/50 sm:start-[50%] sm:translate-x-[-50%]'>
+          <button onClick={onSubmit} className='mt-2'>
+            <svg
+              className='fill-current text-font-console hover:text-white active:text-gray-300'
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+            >
+              <path d='M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z' />
+            </svg>
+          </button>
+        </div>
+
+        <label htmlFor='location-search' className='invisible h-0 w-0'>
+          {/* Label accessible only for people with visual impairment.*/}
+          Search location to navigate.
+        </label>
+        <input
+          id='location-search'
+          type='text'
+          className='invisible z-10 w-[100%] self-stretch rounded-3xl border-none bg-background-console/50 text-left font-semibold placeholder-font-console/90 hover:border-none hover:bg-background-console/90 hover:text-white hover:placeholder-white focus:border-none focus:text-white focus:ring-0 active:placeholder-gray-300 sm:visible'
+          required
+          placeholder='Search...'
+          value={searchValue ?? ''}
+          onChange={onChange}
+          onKeyDown={(evt) => {
+            if (evt.key === 'Enter') {
+              onSubmit();
+            }
+          }}
+        ></input>
       </div>
 
-      <label htmlFor='location-search' className='invisible h-0 w-0'>
-        Search location to navigate.
-      </label>
-      <input
-        id='location-search'
-        type='text'
-        className='invisible z-10 w-[100%] self-stretch rounded-3xl border-none bg-background-console/50 text-left font-semibold placeholder-font-console/90 hover:border-none hover:bg-background-console/90 hover:text-white hover:placeholder-white focus:border-none focus:text-white focus:ring-0 active:placeholder-gray-300 sm:visible'
-        required
-        placeholder='Search...'
-        value={searchValue ?? ''}
-        onChange={onChange}
-        onKeyDown={(evt) => {
-          if (evt.key === 'Enter') {
-            onSubmit();
-          }
-        }}
-      ></input>
-    </div>
+      <div className='visible absolute start-0 z-10 ml-4 mt-6 flex w-[28%] translate-x-0 items-center gap-0 pl-3 pt-1 sm:invisible sm:start-[50%] sm:ml-5 sm:mt-7 sm:translate-x-[-50%] sm:pt-0'>
+        <div className='start-0 z-10 translate-x-0 text-white/50 sm:start-[50%] sm:translate-x-[-50%]'>
+          <button
+            onClick={() => {
+              setShouldOpenSearchBox(!shouldOpenSearchBox);
+            }}
+            className='mt-2'
+          >
+            <svg
+              className='fill-current text-font-console hover:text-white active:text-gray-300'
+              xmlns='http://www.w3.org/2000/svg'
+              width='24'
+              height='24'
+              viewBox='0 0 24 24'
+            >
+              <path d='M23.111 20.058l-4.977-4.977c.965-1.52 1.523-3.322 1.523-5.251 0-5.42-4.409-9.83-9.829-9.83-5.42 0-9.828 4.41-9.828 9.83s4.408 9.83 9.829 9.83c1.834 0 3.552-.505 5.022-1.383l5.021 5.021c2.144 2.141 5.384-1.096 3.239-3.24zm-20.064-10.228c0-3.739 3.043-6.782 6.782-6.782s6.782 3.042 6.782 6.782-3.043 6.782-6.782 6.782-6.782-3.043-6.782-6.782zm2.01-1.764c1.984-4.599 8.664-4.066 9.922.749-2.534-2.974-6.993-3.294-9.922-.749z' />
+            </svg>
+          </button>
+        </div>
+      </div>
+
+      {shouldOpenSearchBox ? (
+        <input
+          id='location-search'
+          type='text'
+          className='absolute left-[50%] z-20 mt-7 w-[50%] translate-x-[-50%] rounded-3xl border-none bg-background-console/90 text-left font-semibold placeholder-font-console/90 hover:border-none hover:bg-background-console hover:text-white hover:placeholder-white focus:border-none focus:text-white focus:ring-0 active:placeholder-gray-300 '
+          required
+          placeholder='Search...'
+          value={searchValue ?? ''}
+          onChange={onChange}
+          onKeyDown={(evt) => {
+            if (evt.key === 'Enter') {
+              onSubmit();
+            }
+          }}
+        ></input>
+      ) : (
+        <></>
+      )}
+    </>
   );
 }
