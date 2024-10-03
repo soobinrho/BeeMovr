c22e1c4e238ca0a612f0e93c733c2189e101d509
Soobin Rho <soobinrho@gmail.com>
2024-10-02 20:44:07 -0500

    fix: eslint error for '
    Signed-off-by: Soobin Rho <soobinrho@gmail.com>

--- src/app/components/api-limit-info-box.tsx
+++ src/app/components/api-limit-info-box.tsx
@@ -14,11 +14,11 @@ export default function ApiLimitInfoBox() {
           <div className='relative mx-4 my-16 whitespace-break-spaces rounded-md bg-background-console/90 p-4 text-left text-sm text-font-console sm:mx-[25%] sm:px-16 sm:py-12 sm:text-base'>
             <p className='font-black'>2024-09-13</p>
             <p className='font-normal'>
-              We've disabled access to our map (powered by Mapbox's free tier
-              APIs) until we implement a feature to limit our website to 50,000
-              loads per month because otherwise we can accidentally be billed
-              $$$$... We will re-enable our map once the rate limit control is
-              in place.
+              We&apos;ve disabled access to our map (powered by Mapbox&apos;s
+              free tier APIs) until we implement a feature to limit our website
+              to 50,000 loads per month because otherwise we can accidentally be
+              billed $$$$... We will re-enable our map once the rate limit
+              control is in place.
             </p>
           </div>
         ) : (
